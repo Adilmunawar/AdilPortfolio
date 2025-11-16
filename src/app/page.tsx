b3ab7ba2bc69b40913e296ecb3ceef5d42680461
Adil Munawar <adilfromavengers@gmail.com>
2025-11-16 17:34:54 +0000

    well remove all the things regarding background and keep is steady plain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,10 @@ import SkillsSection from '@/components/SkillsSection';
 import ServicesSection from '@/components/ServicesSection';
 import ProjectsSection from '@/components/ProjectsSection';
 import ContactSection from '@/components/ContactSection';
-import FloatingLines from '@/components/FloatingLines';
 
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-cyber-dark">
-      <FloatingLines 
-        mixBlendMode='screen'
-        animationSpeed={0.5}
-        interactive={true}
-        lineCount={10}
-        lineDistance={0.3}
-      />
       <Navigation />
       
       <main className="relative z-10">
