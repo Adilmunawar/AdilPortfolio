75f7fe1bab8654455d430bf7ece11f5557fbc2ee
Adil Munawar <adilfromavengers@gmail.com>
2025-11-16 17:27:50 +0000

    well lets change the background I want something instresting on the back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,18 @@ import SkillsSection from '@/components/SkillsSection';
 import ServicesSection from '@/components/ServicesSection';
 import ProjectsSection from '@/components/ProjectsSection';
 import ContactSection from '@/components/ContactSection';
+import FloatingLines from '@/components/FloatingLines';
 
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-cyber-dark">
-      <div className="bg-aurora fixed inset-0 z-0 opacity-40" />
+      <FloatingLines 
+        mixBlendMode='screen'
+        animationSpeed={0.5}
+        interactive={true}
+        lineCount={10}
+        lineDistance={0.3}
+      />
       <Navigation />
       
       <main className="relative z-10">
