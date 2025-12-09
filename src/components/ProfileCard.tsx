640cd110b4d9668bfed8bb09c4fdbd461d1def19
Adil Munawar <adilfromavengers@gmail.com>
2025-12-09 10:48:29 +0000

    the profile card is still looking a bit blue match it with our theme

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -19,8 +19,8 @@ interface ProfileCardProps {
   showUserInfo?: boolean;
   onContactClick?: () => void;
 }
-const DEFAULT_BEHIND_GRADIENT = "radial-gradient(farthest-side circle at var(--pointer-x) var(--pointer-y),hsla(266,100%,90%,var(--card-opacity)) 4%,hsla(266,50%,80%,calc(var(--card-opacity)*0.75)) 10%,hsla(266,25%,70%,calc(var(--card-opacity)*0.5)) 50%,hsla(266,0%,60%,0) 100%),radial-gradient(35% 52% at 55% 20%,#00ffaac4 0%,#073aff00 100%),radial-gradient(100% 100% at 50% 50%,#00c1ffff 1%,#073aff00 76%),conic-gradient(from 124deg at 50% 50%,#c137ffff 0%,#07c6ffff 40%,#07c6ffff 60%,#c137ffff 100%)";
-const DEFAULT_INNER_GRADIENT = "linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)";
+const DEFAULT_BEHIND_GRADIENT = "radial-gradient(farthest-side circle at var(--pointer-x) var(--pointer-y),hsla(189, 84%, 70%,var(--card-opacity)) 4%,hsla(189, 84%, 60%,calc(var(--card-opacity)*0.75)) 10%,hsla(195, 100%, 80%,calc(var(--card-opacity)*0.5)) 50%,hsla(240, 0%, 30%,0) 100%),radial-gradient(35% 52% at 55% 20%,#22d3eec4 0%,#0f172a00 100%),radial-gradient(100% 100% at 50% 50%,#22d3eeff 1%,#0f172a00 76%),conic-gradient(from 124deg at 50% 50%,#22d3eeff 0%,#0891b2ff 40%,#0891b2ff 60%,#22d3eeff 100%)";
+const DEFAULT_INNER_GRADIENT = "linear-gradient(145deg,#0f172a8c 0%,#cffafe44 100%)";
 const ANIMATION_CONFIG = {
   SMOOTH_DURATION: 600,
   INITIAL_DURATION: 1500,
