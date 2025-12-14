6847f65305610d94cf82f566bed65c30f42a5389
Adil Munawar <adilfromavengers@gmail.com>
2025-12-14 11:44:15 +0000

    yeah its looking great now but we can make it more professional if we ad

--- src/components/ui/neon-orbs.tsx
+++ src/components/ui/neon-orbs.tsx
@@ -32,49 +32,6 @@ export function NeonOrbs() {
         </div>
       </div>
 
-      {/* Bottom-center orb */}
-      <div
-        className={`absolute transition-all duration-1000 ease-out delay-300 ${
-          mounted ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
-        }`}
-        style={{
-          bottom: "-50%",
-          left: "50%",
-          transform: "translateX(-50%)",
-          width: "100vw",
-          height: "100vw",
-          maxWidth: "1000px",
-          maxHeight: "1000px",
-        }}
-      >
-        <div className="w-full h-full rounded-full relative orb-light transition-all duration-500">
-          <div className="beam-container beam-spin-10-reverse">
-            <div className="beam-light" />
-          </div>
-        </div>
-      </div>
-
-      {/* Top-right orb */}
-      <div
-        className={`absolute transition-all duration-1000 ease-out delay-500 ${
-          mounted ? "opacity-100 translate-x-0" : "opacity-0 translate-x-10"
-        }`}
-        style={{
-          top: "-30%",
-          right: "-25%",
-          width: "70vw",
-          height: "70vw",
-          maxWidth: "700px",
-          maxHeight: "700px",
-        }}
-      >
-        <div className="w-full h-full rounded-full relative orb-light transition-all duration-500">
-          <div className="beam-container beam-spin-6">
-            <div className="beam-light" />
-          </div>
-        </div>
-      </div>
-
       {/* Bottom-right orb */}
       <div
         className={`absolute transition-all duration-1000 ease-out delay-700 ${
