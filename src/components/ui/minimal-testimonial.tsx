83e031bc8d3850896a24a5252eda54c6d09953a4
Adil Munawar <adilfromavengers@gmail.com>
2025-12-25 18:41:39 +0000

    write something like people I collabed with directly, and and second thi

--- src/components/ui/minimal-testimonial.tsx
+++ src/components/ui/minimal-testimonial.tsx
@@ -11,13 +11,13 @@ const testimonials = [
     image: "/testimonials/AmnaAli.jpg",
   },
   {
-    quote: "I have been profoundly impressed by his remarkable aptitude",
+    quote: "I have been profoundly impressed by his remarkable aptitude for web development and problem-solving.",
     name: "Alice Austen",
     role: "Design Lead at Linear",
     image: "https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDN8fGF2YXRhcnN8ZW58MHx8MHx8fDA",
   },
   {
-    quote: "Collaborating with Adil was seamless,builds high performing sites that make digital marketing actually work. Exceptional developer!",
+    quote: "Collaborating with Adil was seamless. He builds high-performing sites that make digital marketing work.",
     name: "Esha Riaz",
     role: "Digital Marketor",
     image: "/testimonials/esha.jpg",
